910fabe1e6b1389dcb9d258a1443b347a0f064bb
Michael P <michael@otacoo.com>
2012-02-04 00:04:01 +0900

    Revert "Stamp HEAD as 1.0devel."
    This reverts commit d2e24691a95e5cbda8fe60b9c992c4808ba73adf.

--- src/tools/version_stamp.pl
+++ src/tools/version_stamp.pl
@@ -22,8 +22,8 @@
 
 # Major version is hard-wired into the script.  We update it when we branch
 # a new development version.
-$major1 = 1;
-$major2 = 0;
+$major1 = 9;
+$major2 = 1;
 
 # Validate argument and compute derived variables
 $minor = shift;
