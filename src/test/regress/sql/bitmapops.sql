fd2cc85fd25250b4713b78f6fe7b4b6cc2000cc0
Michael P <michael@otacoo.com>
2012-04-10 17:52:33 +0900

    Correct the regression test bitmapops
    An INSERT SELECT that requires a lot of runtime is re-allowed.
    This query runs slowly as it performs on 70,000 rows from a
    distributed table to another but test passes.

--- src/test/regress/sql/bitmapops.sql
+++ src/test/regress/sql/bitmapops.sql
@@ -14,10 +14,9 @@
 
 CREATE TABLE bmscantest (a int, b int, t text);
 
--- PGXCTODO: This query runs slowly
--- INSERT INTO bmscantest
---   SELECT (r%53), (r%59), 'foooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo'
---  FROM generate_series(1,70000) r;
+INSERT INTO bmscantest
+  SELECT (r%53), (r%59), 'foooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo'
+  FROM generate_series(1,70000) r;
 
 CREATE INDEX i_bmtest_a ON bmscantest(a);
 CREATE INDEX i_bmtest_b ON bmscantest(b);
