2822d444a1b596cceedd042c72de5caf25dc2481
Ashutosh Bapat <ashutosh.bapat@enterprisedb.com>
2012-04-17 14:35:57 +0530

    Fix regression differences for xc_FQS and xc_FQS_join tests. It involves follwing changes 1. The Explain output contained costs, which needed to be turned off 2. For DMLs we get the nodeid from the datanodes as xc_node_id column. The    column gets added in the targetlist for DMLs. So, expected output needed    to be changed.

--- src/test/regress/sql/xc_FQS_join.sql
+++ src/test/regress/sql/xc_FQS_join.sql
@@ -54,32 +54,32 @@ select cr_table('tab3_mod', '{1, 2, 3}'::int[], 'modulo(val)', 'as select * from
 select * from tab1_rep, tab2_rep where tab1_rep.val = tab2_rep.val and
 										tab1_rep.val2 = tab2_rep.val2 and
 										tab1_rep.val > 1 and tab1_rep.val < 4;
-explain (num_nodes on, nodes off, verbose on) select * from tab1_rep, tab2_rep where tab1_rep.val = tab2_rep.val and
+explain (costs off, num_nodes on, nodes off, verbose on) select * from tab1_rep, tab2_rep where tab1_rep.val = tab2_rep.val and
 										tab1_rep.val2 = tab2_rep.val2 and
 										tab1_rep.val > 3 and tab1_rep.val < 5;
 select * from tab1_rep natural join tab2_rep 
 			where tab2_rep.val > 2 and tab2_rep.val < 5;
-explain (num_nodes on, nodes off, verbose on) select * from tab1_rep natural join tab2_rep
+explain (costs off, num_nodes on, nodes off, verbose on) select * from tab1_rep natural join tab2_rep
 			where tab2_rep.val > 2 and tab2_rep.val < 5;
 select * from tab1_rep join tab2_rep using (val, val2) join tab3_rep using (val, val2)
 									where tab1_rep.val > 0 and tab2_rep.val < 3; 
-explain (num_nodes on, nodes off, verbose on) select * from tab1_rep join tab2_rep using (val, val2) join tab3_rep using (val, val2)
+explain (costs off, num_nodes on, nodes off, verbose on) select * from tab1_rep join tab2_rep using (val, val2) join tab3_rep using (val, val2)
 							where tab1_rep.val > 0 and tab2_rep.val < 3; 
 select * from tab1_rep natural join tab2_rep natural join tab3_rep
 			where tab1_rep.val > 0 and tab2_rep.val < 3;
-explain (num_nodes on, nodes off, verbose on) select * from tab1_rep natural join tab2_rep natural join tab3_rep
+explain (costs off, num_nodes on, nodes off, verbose on) select * from tab1_rep natural join tab2_rep natural join tab3_rep
 			where tab1_rep.val > 0 and tab2_rep.val < 3;
 -- make sure in Joins which are shippable and involve only one node, aggregates
 -- are shipped to
 select avg(tab1_rep.val) from tab1_rep natural join tab2_rep natural join tab3_rep
 			where tab1_rep.val > 0 and tab2_rep.val < 3;
-explain (num_nodes on, nodes off, verbose on) select avg(tab1_rep.val) from tab1_rep natural join tab2_rep natural join tab3_rep
+explain (costs off, num_nodes on, nodes off, verbose on) select avg(tab1_rep.val) from tab1_rep natural join tab2_rep natural join tab3_rep
 			where tab1_rep.val > 0 and tab2_rep.val < 3;
 -- the two replicated tables being joined do not have any node in common, the
 -- query is not shippable
 select * from tab3_rep natural join tab4_rep
 			where tab3_rep.val > 2 and tab4_rep.val < 5;
-explain (num_nodes on, nodes off, verbose on) select * from tab3_rep natural join tab4_rep
+explain (costs off, num_nodes on, nodes off, verbose on) select * from tab3_rep natural join tab4_rep
 			where tab3_rep.val > 2 and tab4_rep.val < 5;
 
 -- Join involving one distributed and one replicated table, with replicated
@@ -87,7 +87,7 @@ explain (num_nodes on, nodes off, verbose on) select * from tab3_rep natural joi
 -- shippable
 select * from tab1_mod natural join tab1_rep
 			where tab1_mod.val > 2 and tab1_rep.val < 4;
-explain (verbose on, nodes off) select * from tab1_mod natural join tab1_rep
+explain (costs off, verbose on, nodes off) select * from tab1_mod natural join tab1_rep
 			where tab1_mod.val > 2 and tab1_rep.val < 4;
 
 -- Join involving one distributed and one replicated table, with replicated
@@ -95,13 +95,13 @@ explain (verbose on, nodes off) select * from tab1_mod natural join tab1_rep
 -- should not be shippable
 select * from tab1_mod natural join tab4_rep
 			where tab1_mod.val > 2 and tab4_rep.val < 4;
-explain (verbose on, nodes off) select * from tab1_mod natural join tab4_rep
+explain (costs off, verbose on, nodes off) select * from tab1_mod natural join tab4_rep
 			where tab1_mod.val > 2 and tab4_rep.val < 4;
 
 -- Join involving two distributed tables, never shipped
 select * from tab1_mod natural join tab2_mod
 			where tab1_mod.val > 2 and tab2_mod.val < 4;
-explain (verbose on, nodes off) select * from tab1_mod natural join tab2_mod
+explain (costs off, verbose on, nodes off) select * from tab1_mod natural join tab2_mod
 			where tab1_mod.val > 2 and tab2_mod.val < 4;
 
 -- Join involving a distributed table and two replicated tables, such that the
@@ -109,42 +109,47 @@ explain (verbose on, nodes off) select * from tab1_mod natural join tab2_mod
 -- permutations
 select * from tab2_rep natural join tab4_rep natural join tab2_mod
 			where tab2_rep.val > 2 and tab4_rep.val < 4;
-explain (verbose on, nodes off) select * from tab2_rep natural join tab4_rep natural join tab2_mod
+explain (costs off, verbose on, nodes off) select * from tab2_rep natural join tab4_rep natural join tab2_mod
 			where tab2_rep.val > 2 and tab4_rep.val < 4;
 select * from tab4_rep natural join tab2_rep natural join tab2_mod
 			where tab2_rep.val > 2 and tab4_rep.val < 4;
-explain (verbose on, nodes off) select * from tab4_rep natural join tab2_rep natural join tab2_mod
+explain (costs off, verbose on, nodes off) select * from tab4_rep natural join tab2_rep natural join tab2_mod
 			where tab2_rep.val > 2 and tab4_rep.val < 4;
 select * from tab2_rep natural join tab2_mod natural join tab4_rep
 			where tab2_rep.val > 2 and tab4_rep.val < 4;
-explain (verbose on, nodes off) select * from tab2_rep natural join tab2_mod natural join tab4_rep
+explain (costs off, verbose on, nodes off) select * from tab2_rep natural join tab2_mod natural join tab4_rep
 			where tab2_rep.val > 2 and tab4_rep.val < 4;
 
 -- qualifications on distributed tables
 -- In case of 2,3,4 datanodes following join should get shipped completely
 select * from tab1_mod natural join tab4_rep where tab1_mod.val = 1 order by tab1_mod.val2;
-explain (verbose on, nodes off, num_nodes on) select * from tab1_mod natural join tab4_rep where tab1_mod.val = 1 order by tab1_mod.val2;
+explain (costs off, verbose on, nodes off, num_nodes on) select * from tab1_mod natural join tab4_rep where tab1_mod.val = 1 order by tab1_mod.val2;
 -- following join between distributed tables should get FQSed because both of
 -- them reduce to a single node
 select * from tab1_mod join tab2_mod using (val2)
 		where tab1_mod.val = 1 and tab2_mod.val = 2 order by tab1_mod.val2;
-explain (verbose on, nodes off, num_nodes on) select * from tab1_mod join tab2_mod using (val2)
+explain (costs off, verbose on, nodes off, num_nodes on) select * from tab1_mod join tab2_mod using (val2)
 		where tab1_mod.val = 1 and tab2_mod.val = 2 order by tab1_mod.val;
 
 -- JOIN involving the distributed table with equi-JOIN on the distributed column
 -- with same kind of distribution on same nodes.
 select * from tab1_mod, tab3_mod where tab1_mod.val = tab3_mod.val and tab1_mod.val = 1;
-explain (verbose on, nodes off) select * from tab1_mod, tab3_mod
+explain (costs off, verbose on, nodes off) select * from tab1_mod, tab3_mod
 			where tab1_mod.val = tab3_mod.val and tab1_mod.val = 1;
 -- DMLs involving JOINs are not FQSed
-explain (verbose on, nodes off) update tab1_mod set val2 = 1000 from tab2_mod 
+-- We need to just make sure that FQS is not kicking in. But the JOINs can still
+-- be reduced by JOIN reduction optimization. Turn this optimization off so as
+-- to generate plans independent of number of nodes in the cluster.
+set enable_remotejoin to false;
+explain (costs off, verbose on, nodes off) update tab1_mod set val2 = 1000 from tab2_mod 
 		where tab1_mod.val = tab2_mod.val and tab1_mod. val2 = tab2_mod.val2;
-explain (verbose on, nodes off) delete from tab1_mod using tab2_mod
+explain (costs off, verbose on, nodes off) delete from tab1_mod using tab2_mod
 		where tab1_mod.val = tab2_mod.val and tab1_mod.val2 = tab2_mod.val2;
-explain (verbose on, nodes off) update tab1_rep set val2 = 1000 from tab2_rep
+explain (costs off, verbose on, nodes off) update tab1_rep set val2 = 1000 from tab2_rep
 		where tab1_rep.val = tab2_rep.val and tab1_rep.val2 = tab2_rep.val2;
-explain (verbose on, nodes off) delete from tab1_rep using tab2_rep 
+explain (costs off, verbose on, nodes off) delete from tab1_rep using tab2_rep 
 		where tab1_rep.val = tab2_rep.val and tab1_rep.val2 = tab2_rep.val2;
+reset enable_remotejoin;
 
 drop table tab1_rep;
 drop table tab2_rep;
