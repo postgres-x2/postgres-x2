41497f76cea9eae378974c0af7f050fda35dca3c
Michael Paquier <michael@otacoo.com>
2012-05-04 16:56:44 +0900

    Fix for test plpgsql
    The same issue as in commit 8e393db79e43f0a5605fed35d55af8375b4ffe2c
    is not fixed for this release. It is an error with query parameters
    of sub-plans not being scanned for SetRemoteStatementName.

--- src/pl/plpgsql/src/pl_exec.c
+++ src/pl/plpgsql/src/pl_exec.c
@@ -3001,6 +3001,15 @@ exec_stmt_execsql(PLpgSQL_execstate *estate,
 						p->commandType == CMD_UPDATE ||
 						p->commandType == CMD_DELETE)
 						stmt->mod_stmt = true;
+#ifdef PGXC
+					/* PGXCTODO: Support a better parameter interface for XC with DMLs */
+					if (p->commandType == CMD_INSERT ||
+						p->commandType == CMD_UPDATE ||
+						p->commandType == CMD_DELETE)
+						ereport(ERROR,
+								(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+								 errmsg("Postgres-XC does not support DML queries in PL/pgSQL")));
+#endif
 				}
 			}
 		}
