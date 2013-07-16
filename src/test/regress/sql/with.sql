d49ca7c5e87ec0acd036208050ea05e758e47c2b
Koichi Suzuki <koichi.clarinet@gmail.com>
2013-07-16 18:15:48 +0900

    This fixes with regression test for 1.1.

--- src/test/regress/sql/with.sql
+++ src/test/regress/sql/with.sql
@@ -589,7 +589,7 @@ with
 A as ( select q2 as id, (select q1) as x from int8_tbl ),
 B as ( select id, row_number() over (partition by id) as r from A ),
 C as ( select A.id, array(select B.id from B where B.id = A.id) from A )
-select * from C;
+select * from C order by id;
 
 --
 -- Test CTEs read in non-initialization orders
