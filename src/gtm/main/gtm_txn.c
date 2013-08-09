9ffefb6a3acec8e85769e519e32c25c3a4b3ac2a
Koichi Suzuki <koichi.clarinet@gmail.com>
2013-08-09 16:59:51 +0900

    This commit fixes potential bug of GTM proxy to handle disconnect.
    Andcrei Martsinchyk reported the bug and provided the fix.

--- src/gtm/main/gtm_txn.c
+++ src/gtm/main/gtm_txn.c
@@ -157,7 +157,12 @@ GTM_GXIDToHandle(GlobalTransactionId gxid)
 	if (gtm_txninfo != NULL)
 		return gtm_txninfo->gti_handle;
 	else
+	{
+		ereport(WARNING,
+				(ERANGE, errmsg("No transaction handle for gxid: %d",
+								gxid)));
 		return InvalidTransactionHandle;
+	}
 }
 
 /*
