8875ab03a8c17b66fc7c4d272f05c2b3aa626151
Koichi Suzuki <koichi.dbms@gmail.com>
2014-03-04 09:42:55 +0900

    A fix of the merge with PostgreSQL 9.3.2.
    Fix the performance drop in DBT-1.

--- src/backend/optimizer/path/joinpath.c
+++ src/backend/optimizer/path/joinpath.c
@@ -259,6 +259,15 @@ add_paths_to_joinrel(PlannerInfo *root,
 							 restrictlist, jointype,
 							 sjinfo, &semifactors,
 							 param_source_rels, extra_lateral_rels);
+#ifdef PGXC
+	/*
+	 * If the inner and outer relations have RemoteQuery paths, check if this
+	 * JOIN can be pushed to the data-nodes. If so, create a RemoteQuery path
+	 * corresponding to the this JOIN.
+	 */
+	create_joinrel_rqpath(root, joinrel, outerrel, innerrel, restrictlist,
+								jointype, sjinfo, param_source_rels);
+#endif /* PGXC */
 }
 
 /*
